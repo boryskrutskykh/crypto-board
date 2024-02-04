aa12dc91734a69c4b214a8c98ced15432f8cc694
borys_krutskykh <borys_krutskykh@epam.com>
2024-02-05 00:01:41 +0200

    Update CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.enableCors();
 
   const config = new DocumentBuilder()
