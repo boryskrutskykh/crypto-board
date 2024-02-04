48f862268c8c1a272215043e51c8a937230311b9
borys_krutskykh <borys_krutskykh@epam.com>
2024-02-05 00:03:32 +0200

    Update CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: true,
+    cors: {
+      origin: 'https://codeplace.xyz',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept',
+    },
   });
   app.enableCors();
 
